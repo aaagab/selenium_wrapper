6bd22f5266d26246749c6a6e4fa82bb1a23d9565
gabaaa <not-set>
2020-06-25 20:12:41 -0500

    added extension support for site-improve and accessibility arg, windows library has also been added as a member of srv

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import sys
 import subprocess
 import shlex
 
+# pip3 install pyautogui
+
 # mklink /H "C:\Users\john\Desktop\data\bin\selenium.py" "C:\Users\john\Desktop\data\bin\selenium\main.py"
 
 if __name__ == "__main__":
@@ -25,6 +27,7 @@ if __name__ == "__main__":
     debug=args.debug.here
     direpa_media="Y:\\bin\\selenium_media"
 
+
     if args.selenium_options.here:
         srv=pkg.SeleniumServer(debug=debug, direpa_media=direpa_media)
         print()
@@ -40,6 +43,11 @@ if __name__ == "__main__":
         cmd=[ srv.filenpa_java, "-jar", srv.filenpa_selenium_server, "-role", "node", "-help" ]
         subprocess.run(cmd)
         sys.exit(0)
+    elif args.examples.here:
+        print("""
+        main.py --connect --driver chrome --accessibility --refresh --url https://www.example.com/e/example/login
+        """)
+        sys.exit(0)
     elif args.gui.here:
         srv=pkg.SeleniumServer(debug=debug, direpa_media=direpa_media)
         srv.show_gui()
@@ -52,9 +60,17 @@ if __name__ == "__main__":
         srv=pkg.SeleniumServer(debug=debug, direpa_media=direpa_media)
         srv.reset(args.drivers.values)
 
-    if args.connect.here:
-        srv=pkg.SeleniumServer(debug=debug, direpa_media=direpa_media)
-        srv.connect(args.driver.value, reset=args.reset.here)
+    if args.connect.here or args.accessibility.here:
+        srv=pkg.SeleniumServer(accessibility=args.accessibility.here, debug=debug, direpa_media=direpa_media)
+        cmd_pid=None
+
+        if args.cmd.here:
+            cmd_pid=srv.windows.get_active()
+
+        if args.connect.here:
+            srv.connect(args.driver.value, reset=args.reset.here)
+        elif args.accessibility.here:
+            srv.connect("chrome", reset=args.reset.here)
 
         if args.focus.here:
             srv.browser_focus()
@@ -68,10 +84,30 @@ if __name__ == "__main__":
         if args.scroll.here:
             srv.get_driver().scroll(percent=args.scroll.value)
 
+        if args.accessibility.here:
+            import pyautogui
+            # you have to take a screenshot of the button
+            extn = pyautogui.locateOnScreen(os.path.join(srv.driver_data["direpa_extensions"], "site_improve_button.png"))
+            offset=15
+            if extn is None:
+                print("Not Found button.png")
+                sys.exit(1)
+            pyautogui.click(x=extn[0]+offset,y=extn[1]+offset,clicks=1,interval=0.0,button="left")
+
+
         if args.driver_info.here:
             pprint(srv.get_driver().dy)
 
+        if args.cmd.here:
+            srv.windows.focus(cmd_pid)
 
+   
+
+
+      
+
+
+        # srv.window_focus("cmd.exe")
         # srv.window_focus("putty.exe")
         # srv.window_focus("Code.exe")
 
