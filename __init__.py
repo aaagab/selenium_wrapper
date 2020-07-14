134ad0e1c79ab576d1b35befcf463750922d17cb
gabaaa <gabaaa.dev@gmail.com>
2020-07-14 12:47:47 -0500

    added netstat back to processes to check on selenium browsers when --reset

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 4.5.1
+# version: 4.5.2
 # name: release
 # license: MIT
 
-__version__ = "4.5.1"
+__version__ = "4.5.2"
 
 from .dev.obj_info import get_obj_info
 from .dev.server import SeleniumServer
