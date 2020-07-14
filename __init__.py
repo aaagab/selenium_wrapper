c24f5fb59423bbb1f2942d4ed3fba831cfb737fb
gabaaa <gabaaa.dev@gmail.com>
2020-07-14 09:43:44 -0500

    solved issue ie  Protected Mode settings are not the same for all zones.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 4.5.0
+# version: 4.5.1
 # name: release
 # license: MIT
 
-__version__ = "4.5.0"
+__version__ = "4.5.1"
 
 from .dev.obj_info import get_obj_info
 from .dev.server import SeleniumServer
