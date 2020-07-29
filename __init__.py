92d17055aa9c7874eba3e307c000bcce87e1a31d
gabaaa <gabaaa.dev@gmail.com>
2020-07-29 15:07:48 -0500

    added bypass insecure certificate

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 4.5.3
+# version: 4.6.0
 # name: release
 # license: MIT
 
-__version__ = "4.5.3"
+__version__ = "4.6.0"
 
 from .dev.obj_info import get_obj_info
 from .dev.server import SeleniumServer
