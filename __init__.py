169fc7067210bb84a9dac07e50aa438785a191a8
gabaaa <gabaaa.dev@gmail.com>
2022-03-11 15:50:13 -0600

    updated accessibility site improve extension to v2

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: "6.1.1"
+# version: "6.2.0"
 # name: release
 # license: MIT
 
-__version__= "6.1.1"
+__version__= "6.2.0"
 
 from .dev.obj_info import get_obj_info
 from .dev.server import SeleniumServer
