2d853af9a6809dcc80a2bce3b9c1b6291ce7edc6
gabaaa <not-set>
2023-03-14 16:40:01 -0500

    internally get_elem has been modified and querySelectorAll has been added, for arguments click and select

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: "8.0.1"
+# version: "9.0.0"
 # name: release
 # license: MIT
 
-__version__= "8.0.1"
+__version__= "9.0.0"
 
 from .dev.obj_info import get_obj_info
 from .dev.server import SeleniumServer
