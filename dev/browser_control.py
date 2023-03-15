f1f018d334991ed0820fb9c8001f114b59d57b9f
gabaaa <gabaaa.dev@gmail.com>
2023-03-15 11:39:26 -0500

    for click and select, queryselectorall has been replaced by xpath

--- dev/browser_control.py
+++ dev/browser_control.py
@@ -17,54 +17,72 @@ from ..gpkgs import message as msg
 def get_elem(
     driver, 
     id=None,
-    query=None, 
-    query_index=None,
+    xpath=None, 
+    xpath_context=None,
     wait_ms=2000,
     error=True
 ):
     timer=TimeOut(wait_ms, unit="milliseconds").start()
 
-    if id is None and query is None:
-        msg.error("When finding an element, at id or query must be provided", trace=True, exit=1)
+    if id is None and xpath is None:
+        msg.error("When finding an element, at id or xpath must be provided", trace=True, exit=1)
 
-    if query_index is not None:
-        if isinstance(query_index, int):
-            if query_index < 0:
-                msg.error("For element '{}' query_index '{}' must be a positive integer.".format(id, query_index), exit=1)
-        else:
-            msg.error("For element '{}' query_index '{}' must be of type {}.".format(id, query_index, int), exit=1)
+    if xpath_context is None:
+        xpath_context="document"
 
     while True:
         if timer.has_ended(pause=.001):
             if error is True:
-                msg.error("element not found '{}'".format(id))
+                if id is None:
+                    msg.error("element not found with xpath '{}'".format(xpath))
+                else:
+                    msg.error("element not found with id '{}'".format(id))
                 sys.exit(1)
             else:
                 return None
         
-        if query is None:
+        if xpath is None:
             try:
                 return driver.find_element(By.ID, id)
             except NoSuchElementException as e:
                 pass
         else:
-            if len(query) >=2:
-                if query[0] == "'" and query[-1] == "'":
-                    query=query[1:len(query)-1]
-                    query=query.replace("\"", "\\\"")
+            if len(xpath) >=2:
+                if xpath[0] == "'" and xpath[-1] == "'":
+                    xpath=xpath[1:len(xpath)-1]
+                    xpath=xpath.replace("\"", "\\\"")
 
             try:
-                elems=(driver.execute_script("return document.querySelectorAll(\"{}\")".format(query)))
-                if elems is not None:
-                    if query_index is None:
-                        return elems[0]
-                    else:
-                        if query_index > len(elems) -1:
-                            msg.error("For element '{}' query_index '{}' is greater than elements length '{}'.".format(id, query_index, len(elems)), exit=1)
-                        else:
-                            return elems[query_index]
+                elems=driver.execute_script("""
+                    results=document.evaluate(\"{}\", {}, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+                    var elements = []; 
+                    var element= results.iterateNext(); 
+                    while (element) {{
+                        elements.push(element);
+                        element = results.iterateNext();
+                    }}
+                    return elements;
+                """.format(
+                    xpath,
+                    xpath_context,
+                ))
+
+                if len(elems) == 1:
+                    return elems[0]
+                elif len(elems) > 0:
+                    msg.error("'{}' elements have been found but only one needs to be selected. (use xpath index notation)".format(len(elems)))
+                    index=1
+                    for elem in elems:
+                        print("    {}- tag={} text={}".format(
+                            index,
+                            elem.tag_name,
+                            elem.text,
+                        ))
+                        index+=1
+                    sys.exit(1)
+            
             except JavascriptException:
-                msg.error("For element '{}' wrong syntax at javascript querySelectorAll query '{}'.".format(id, query))
+                msg.error("Wrong javascript syntax for xpath '{}' and context '{}'.".format(xpath, xpath_context))
                 raise
 
     return None
