45006637d5576dd6b55197c4804ae2ffab04a2c4
gabaaa <gabaaa.dev@gmail.com>
2023-07-11 10:59:18 -0500

    fix

--- __init__.py
+++ __init__.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: "13.1.0"
+# version: "13.1.1"
 # name: release
 # license: MIT
 
-__version__= "13.1.0"
+__version__= "13.1.1"
 
 from .dev.obj_info import get_obj_info
 from .dev.server import SeleniumServer, SeleniumSettings
+from .dev.browser_control import ElementNotFound
 from .gpkgs.geturl import geturl
 from .gpkgs import message as msg
 from .gpkgs.nargs import Nargs
 from .gpkgs import shell_helpers as shell
 from .gpkgs.getpath import getpath 
-
 
 # from .dev.processes import Processes
