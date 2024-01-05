290d1d15954c99e41786014794ec2e1ab008ef60
gabaaa <gabaaa.dev@gmail.com>
2024-01-05 17:10:38 -0600

    fix

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: "13.1.2"
+# version: "13.1.3"
 # name: release
 # license: MIT
 
-__version__= "13.1.2"
+__version__= "13.1.3"
 
 from .dev.obj_info import get_obj_info
 from .dev.server import SeleniumServer, SeleniumSettings
