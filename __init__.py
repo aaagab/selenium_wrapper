2872eb1ea14f52e83c346630c90b2eaf0b5eb311
gabaaa <not-set>
2024-02-21 16:34:03 -0600

    Porting to Linux

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: "13.2.1"
+# version: "13.2.2"
 # name: release
 # license: MIT
 
-__version__= "13.2.1"
+__version__= "13.2.2"
 
 from .dev.obj_info import get_obj_info
 from .dev.server import SeleniumServer, SeleniumSettings
